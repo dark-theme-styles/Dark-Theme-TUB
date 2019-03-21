833c68d0d6f299ba19473a81d0e6ad07d5155b79
B4rtware <hansfrans47@hotmail.de>
2019-03-21 15:51:08 +0100

    fixed pre-commit hook and added emojis 😏

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,11 +1,17 @@
-#!/bin/sh
-echo "[OK] creating todos ..."
+#!/bin/bash
+# define emojis here because they are somehow bugged if you use whitespace after inserting them
+work_emoji="⚙️ "
+check_emoji="✔️ "
+error_emoji="❌ "
+
+# create the todos json file
+echo "$work_emoji creating todos ..."
 leasot --reporter json **/*.css > todos.json
-# check if file exists
-if [ ! -f todos.json]; then
-	echo "File not found!"
+# check if json file exists
+if [ ! -f todos.json ] ; then
+	echo "$error_emoji <todos.json> file not found!"
 	exit 1
 fi
 # add the created file to the commit
 git add todos.json
-echo "✔️   added <todos.json> to commit"
+echo "$check_emoji added <todos.json> to commit"
