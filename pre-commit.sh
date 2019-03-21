46b97193ddc5dbc53d4fb26cfaabbddc849464b2
B4rtware <hansfrans47@hotmail.de>
2019-03-21 17:48:30 +0100

    fixed path

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # define emojis here because they are somehow bugged if you use whitespace after inserting them
 work_emoji="⚙️ "
 check_emoji="✔️ "
@@ -7,11 +7,15 @@ error_emoji="❌ "
 # create the todos json file
 echo "$work_emoji creating todos ..."
 leasot --reporter json **/*.css > docs/todos.json
+# move into docs direcory
+cd docs
 # check if json file exists
-if [ ! -f docs/todos.json ] ; then
+if [ ! -f todos.json ] ; then
 	echo "$error_emoji <todos.json> file not found!"
 	exit 1
 fi
+# move out of folder 
+cd ..
 # add the created file to the commit
-git add todos.json
+git add docs/todos.json
 echo "$check_emoji added <todos.json> to commit"
