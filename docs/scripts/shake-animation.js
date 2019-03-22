9ed1b5251cb9df3e32224591daf63e05f038d243
Re-Krass <38668040+Re-Krass@users.noreply.github.com>
2019-03-22 23:02:22 +0100

    fix shake animation

--- docs/scripts/shake-animation.js
+++ docs/scripts/shake-animation.js
@@ -3,5 +3,8 @@ icon.addEventListener("click", () => {
     var animation_elements = document.getElementsByClassName("shake-animation");
     for (let anim_elem of animation_elements) {
         anim_elem.classList.toggle("shake-animation-active");
+        setTimeout(() => {
+            anim_elem.classList.remove("shake-animation-active");
+        }, 300)
     }
 });
