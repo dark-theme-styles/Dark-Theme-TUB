6a913d2f820b4bb78542b4727bb12d0f4084db1f
B4rtware <hansfrans47@hotmail.de>
2019-04-07 19:46:39 +0200

    start implement todo view

---
+++ dark-theme-tub/src/components/todo/TodoLine.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-expansion-panel>
+    <v-expansion-panel-content>
+      <template v-slot:header>
+        <v-container
+          fluid
+          pa-0
+        >
+          <v-layout>
+            <v-flex
+              xs1
+              offset-xs2
+            >
+              <v-card
+                color="error"
+                height="100%"
+                flat
+                tile
+              >
+                <v-card-text>line {{line}}</v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex xs1>
+              <v-card
+                color="success"
+                height="100%"
+                flat
+                tile
+              >
+                <v-card-text>{{todoType}}</v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex xs8>
+              <v-card
+                color="info"
+                height="100%"
+                flat
+                tile
+              >
+                <v-card-text>{{comment}}</v-card-text>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </template>
+      <v-card color="info">
+        <pre><code>Das ist code</code></pre>
+      </v-card>
+    </v-expansion-panel-content>
+  </v-expansion-panel>
+</template>
+
+<script>
+export default {
+    name: "TodoLine",
+    props: {
+        line: Number,
+        todoType: {
+            type: String,
+            validator: val => ["TODO", "FIXME"].includes(val)
+        },
+        comment: String
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+#first {
+    background-color: red;
+}
+
+::deep .v-expansion-panel__header {
+  padding: 0;
+}
+</style>
+
--- dark-theme-tub/src/views/Todo.vue
+++ dark-theme-tub/src/views/Todo.vue
@@ -1,5 +1,33 @@
 <template>
-    <div class="about">
-        <h1>This is the Todos page</h1>
-    </div>
+  <div id="todo">
+    <v-expansion-panel :value="1">
+      <v-expansion-panel-content>
+        <template v-slot:header>
+          <div>Item</div>
+        </template>
+        <TodoLine
+          :line="4"
+          :todo-type="'TODO'"
+          :comment="'Das ist ein kleines Kommentar mal sehen wie es ist wenn es zu lang ist'"
+        />
+        <TodoLine
+          :line="4"
+          :todo-type="'TODO'"
+          :comment="'Das ist ein kleines Kommentar mal sehen wie es ist wenn es zu lang ist'"
+        />
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </div>
 </template>
+
+<script>
+import TodoLine from "@/components/todo/TodoLine";
+
+export default {
+    name: "Todo",
+    components: {
+        TodoLine
+    }
+};
+</script>
+
