25938ed634c1e6903cf8b80a082f480db80a5807
Re-Krass <38668040+Re-Krass@users.noreply.github.com>
2019-04-17 21:11:27 +0200

    maybe fix build vue

---
+++ dark-theme-tub/vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  assetsDir: undefined,
+  publicPath: '',
+  outputDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+}
