095b35edbafb59664e655aa75cdaea89cdecb9d7
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-04-17 23:51:48 +0200

    removed forwardslash from base url

--- dark-theme-tub/src/router.js
+++ dark-theme-tub/src/router.js
@@ -6,7 +6,7 @@ Vue.use(Router);
 
 export default new Router({
     mode: "history",
-    base: "https://dark-theme-styles.github.io/Dark-Theme-TUB/",
+    base: "https://dark-theme-styles.github.io/Dark-Theme-TUB",
     routes: [
         {
             path: "/",
